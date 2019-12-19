2d225fa76c3bfeb15d6dfe335f5c7bc7cc6ad3b1
Trey <trey@lavidadev.com>
2019-12-19 05:28:52 -0500

    Add create event component

---
+++ src/components/CreateEvent/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Axios from 'axios';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
+class CreateForm extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+  
+  
+    render() {
+      return (
+        <section className="create-info-wrapper">
+            <Link to="/">Go Home</Link>
+        </section>
+      );
+    }
+  }
+
+  export default CreateForm;
