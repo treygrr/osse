827ad27f4356eb641ab70c1c070c4a8e38a65a5b
Trey <trey@lavidadev.com>
2019-12-19 22:31:33 -0500

    Refactor header header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter, Route, Link} from 'react-router-dom';
 
 import CreateForm from '../CreateForm';
-import CreateEvent from '../CreateEvent';
-
+import CreateEvent from '../CreateEvent/CreateEvent';
+import Header from './Header';
 const App = () => {
   return (
     <div className="App">
-        <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <BrowserRouter>
-            <div>
-              <Route path="/" exact component={CreateForm} />
-              <Route path="/create" component={CreateEvent} />
-            </div>
-          </BrowserRouter>
+      <BrowserRouter>
+      <Header />
+        <div className="App-body">
+          <Route path="/" exact component={CreateForm} />
+          <Route path="/create" component={CreateEvent} />
         </div>
+      </BrowserRouter>
     </div>
   );
 }
