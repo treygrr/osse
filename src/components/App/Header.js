7890b546e5312ca8c265d2321839b53ab53b0e74
Trey <trey@lavidadev.com>
2019-12-19 22:33:09 -0500

    Add new header component

---
+++ src/components/App/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../App/logo.svg';
+import lantern from '../App/lantern.svg'
+import github from '../App/github.svg'
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div className="App-header">
+                <div className="logo-wrapper">
+                    <img src={logo} className="App-logo App-header-item" alt="logo" />
+                    <img src={lantern} className="App-logo-lantern" alt="lantern" />
+                </div>
+                
+                <div>OSRS Skilling Events</div>
+                <a href="https://github.com/treygrr/osse"><img src={github} className="App-logo-github" alt="github" /></a>
+            </div>
+        );
+    }
+
+}
+
+export default Header;
