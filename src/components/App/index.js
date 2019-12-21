ee4eca2b78a66fc114d15ec62904a38fea07e3a9
Trey <trey@lavidadev.com>
2019-12-21 08:29:09 -0500

    Attempt fixing component updating

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,18 +6,35 @@ import { BrowserRouter, Route, Link} from 'react-router-dom';
 import CreateForm from '../CreateForm';
 import CreateEvent from '../CreateEvent/CreateEvent';
 import Header from './Header';
-const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Header />
-        <div className="App-body">
-          <Route path="/" exact component={CreateForm} />
-          <Route path="/create" component={CreateEvent} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      names: [],
+      addMember: 'tea'
+    };
+  }
+
+  appStateUpdate(data) {
+    this.setState({
+      names: data
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <BrowserRouter>
+        <Header />
+          <div className="App-body">
+            <Route path="/" exact render={(props) => <CreateForm data={this.appStateUpdate.bind(this)}/>}/>
+            <Route path="/create" component={CreateEvent} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
