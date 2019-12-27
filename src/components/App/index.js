cf385f869d5dc46c9a13eeda482262ee60641b7b
Trey <trey@lavidadev.com>
2019-12-27 04:50:21 -0500

    Add state properties to routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import './App.css';
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import CreateForm from '../CreateForm';
-import CreateEvent from '../CreateEvent/CreateEvent';
+import ViewEvent from '../CreateEvent/ViewEvent';
 import EventList from '../CreateEvent/EventList';
 import Header from './Header';
 
@@ -13,25 +13,35 @@ class App extends React.Component {
     super();
     this.state = { 
       names: [],
-      addMember: 'Typer Username'
+      addMember: 'Typer Username',
+      selection: null,
     };
   }
 
   appStateUpdate(data) {
-    this.setState({
-      names: data
-    });
+    if (data){
+      this.setState({
+        names: data
+      });
+    }
+  }
+  updateSelection(selected){
+    if (selected){
+      this.setState({
+        selection: selected
+      });
+    }
   }
   render(){
     return (
       <div className="App">
         <BrowserRouter>
         <Header />
-        <EventList></EventList>
+        <EventList  data={ {updateSelection: this.updateSelection.bind(this)} } selectionName={this.state.selection}>
+        </EventList>
           <div className="App-body">
-            
-            <Route path="/create" component={CreateEvent} />
-            <Route path="/" exact render={(props) => <CreateForm data={{appStateUpdate:this.appStateUpdate.bind(this), names: this.state.names, addMember: this.state.addMember}}/>}/>
+            <Route path="/view" render={(props) => <ViewEvent selectionName={this.state.selection}  names={this.state.names}/>} />
+            <Route path="/" exact render={(props) => <CreateForm data={{appStateUpdate:this.appStateUpdate.bind(this), updateSelection: this.updateSelection.bind(this), names: this.state.names, addMember: this.state.addMember}}/>}/>
           </div>
         </BrowserRouter>
       </div>
