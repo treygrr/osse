d2427ebe026cfacad48342bdc44bcebe377a2164
Trey <trey@lavidadev.com>
2019-12-27 04:49:44 -0500

    Update links to use react router

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../App/logo.svg';
 import lantern from '../App/lantern.svg'
 import github from '../App/github.svg'
@@ -9,11 +10,11 @@ class Header extends React.Component {
         return (
             <div className="App-header">
                 <div className="logo-wrapper">
-                    <img src={logo} className="App-logo App-header-item" alt="logo" />
+                    <Link to="/"><img src={logo} className="App-logo App-header-item" alt="logo" /></Link>
                     <img src={lantern} className="App-logo-lantern" alt="lantern" />
                 </div>
                 
-                <p>OSRS:SE</p>
+                <a href="/">OSRS:SE</a>
                 <a href="https://github.com/treygrr/osse"><img src={github} className="App-logo-github" alt="github" /></a>
             </div>
         );
