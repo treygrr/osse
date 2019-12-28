a538d03a0fa7ae3e28eeac1590acbc8fe175b529
Trey <trey@lavidadev.com>
2019-12-28 11:53:10 -0500

    File Cleanup

--- src/components/CreateEvent/EventList.js
+++ src/components/CreateEvent/EventList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../CreateEvent/EventList.css';
+
 class EventList extends React.Component {
     constructor(props){
         super(props);
