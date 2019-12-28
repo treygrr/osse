5028daeced4758ba6281792614aed451743ce6aa
Trey <trey@lavidadev.com>
2019-12-28 11:53:38 -0500

    Create getCurrent wrapper for axios

---
+++ src/components/CreateEvent/GetCurrent.js
@@ -0,0 +1,31 @@
+import Axios from 'axios';
+class Current {
+    constructor(){
+        this.url = 'http://localhost:3001/skillevent/create';
+        this.axios = new Axios.create();
+    }
+
+    async getUserData(username) {
+        const response = await this.axios.get(this.url, {
+            params: {
+                username: username
+            }
+        });
+/*         .then(async(response) => {
+        if (response.status === 200) {
+            console.log(response.data);
+            return response.data;
+        }
+        })
+        .catch(async(error) => {
+        if (error.status !== 200) {
+            console.log(error);
+            return;
+        }
+        console.log(error);
+        }); */
+        return response;
+    }
+}
+
+export default Current;
