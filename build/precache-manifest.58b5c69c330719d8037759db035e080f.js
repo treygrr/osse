324fb85aebdc936b24af3992c18abc7209b71c86
Trey <trey@lavidadev.com>
2019-12-29 05:26:56 -0500

    Add production build

---
+++ build/precache-manifest.58b5c69c330719d8037759db035e080f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0902cb6a27da25d22d45b7499d3b9c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbb19dea99863a7805b",
+    "url": "/static/css/main.f58a8869.chunk.css"
+  },
+  {
+    "revision": "663fd4ffa8303418e1a7",
+    "url": "/static/js/2.a641b368.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.a641b368.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dbb19dea99863a7805b",
+    "url": "/static/js/main.1d23dc04.chunk.js"
+  },
+  {
+    "revision": "44d6ca4837bc1fd4f9fb",
+    "url": "/static/js/runtime-main.3e49b20a.js"
+  },
+  {
+    "revision": "e92ff569bae5fdf7b2942c38d867f8b0",
+    "url": "/static/media/clicktosee.e92ff569.png"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "5822536919887bea63da71b5ced38d61",
+    "url": "/static/media/lantern.58225369.svg"
+  },
+  {
+    "revision": "8f70103c1c8a49d5455f4207e46ed4f0",
+    "url": "/static/media/logo.8f70103c.svg"
+  }
+]);
